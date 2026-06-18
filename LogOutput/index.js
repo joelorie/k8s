8cd97d6e0da7abeb44ca017a656d1b8eae1d7353
owalkz <jjaraorie@gmail.com>
2026-06-18 15:57:58 +0300

    Created Exercise 1.1 Files Includes Dockerfile

---
+++ LogOutput/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs/promises')
+
+const logFilePath = path.join(__dirname, 'application.log')
+
+const app = express()
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`Application running on port ${PORT}`)
+  generateRandomString()
+})
+
+const generateRandomString = async () => {
+  setInterval(async () => {
+    let randomString = crypto.randomUUID()
+    let timeStamp = new Date().toISOString()
+    console.log(timeStamp + ': ' + randomString)
+    await fs.appendFile(logFilePath, randomString.concat('\n'), 'utf-8')
+  }, 5000)
+}
